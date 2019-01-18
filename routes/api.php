fb7072082c6e90cc11f7f40596eb10824f6aca03
Malay Mehta <meetmalay@gmail.com>
2019-01-18 12:05:17 +0530

    - Added proper comments in the api routes - Added new package for handling the CORS

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,17 @@ use Illuminate\Http\Request;
 |
 */
 
+// Get the list of all the apartments
 Route::get('apartments', 'Api\ApartmentsController@index');
+
+// Get the information about one apartment based on the token
 Route::get('apartment/{access_token}', 'Api\ApartmentsController@show');
+
+// Delete one apartment based on the token
 Route::get('apartment/delete/{access_token}', 'Api\ApartmentsController@destroy');
 
+// Store New Apartment
 Route::post('apartment/store', 'Api\ApartmentsController@store');
+
+// Update Existing Apartment Based on Token
 Route::post('apartment/update/{access_token}', 'Api\ApartmentsController@update');
